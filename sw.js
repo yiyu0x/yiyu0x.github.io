a2dfcdb309403da3eaf3d2d8258ed992470ea548
Kyle Chang <hello@yiyu0x.org>
2025-04-12 22:35:24 +0000

    Site updated: 2025-04-12 22:35:24

--- sw.js
+++ sw.js
@@ -1,14 +1,1 @@
-<html><head></head><body>importScripts('https://g.alicdn.com/kg/workbox/3.3.0/workbox-sw.js');
-
-if (workbox) {
-    workbox.setConfig({ modulePathPrefix: 'https://g.alicdn.com/kg/workbox/3.3.0/' });
-
-    workbox.precaching.precache(['/', '/index.html']);
-
-    workbox.routing.registerRoute(new RegExp('^https?://blog.yiyu0x.org/?$'), workbox.strategies.networkFirst());
-
-    workbox.routing.registerRoute(new RegExp('.*.html'), workbox.strategies.networkFirst());
-
-    workbox.routing.registerRoute(new RegExp('.*.(?:js|css|jpg|png|gif)'), workbox.strategies.staleWhileRevalidate());
-
-}</body></html>
+var CACHE_NAME="1744497200477",urlsToCache=["/"];self.addEventListener("install",function(e){e.waitUntil(caches.open(CACHE_NAME).then(function(e){return e.addAll(urlsToCache)}))}),self.addEventListener("activate",function(e){e.waitUntil(caches.keys().then(function(e){return Promise.all(e.map(function(e){if(e!==CACHE_NAME)return caches.delete(e)}))}))}),self.addEventListener("fetch",function(n){n.respondWith(caches.match(n.request).then(function(e){if(e)return e;var t=n.request.clone();return fetch(t).then(function(e){if(!e||200!==e.status||"basic"!==e.type)return e;var t=e.clone();return caches.open(CACHE_NAME).then(function(e){e.put(n.request,t)}),e})}))});
